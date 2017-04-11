470f42d328a2245fdfd89e2d492bc6077ab7ee00
Buys Hans <buyshans@gmail.com>
2017-04-11 23:21:52 +0200

    initial commit: added solution with empty xamarin forms projects

---
+++ Ehb.Dijlezonen.Kassa.App/Ehb.Dijlezonen.Kassa.App.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ehb.Dijlezonen.Kassa.App.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new Shared.App());
+        }
+    }
+}
---
+++ Ehb.Dijlezonen.Kassa.App/Ehb.Dijlezonen.Kassa.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ehb.Dijlezonen.Kassa.App.Shared
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
