9c861dee047109022fd505020b5010455301b699
Buys Hans <buyshans@gmail.com>
2017-04-13 23:23:44 +0200

    removed dead project

--- Ehb.Dijlezonen.Kassa.App.Android.Tests/ILogAssertions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ehb.Dijlezonen.Kassa.Infrastructure.Testing;
-using log4net;
-
-namespace Ehb.Dijlezonen.Kassa.App.Android.Tests
-{
-    internal class LogAssertions : Assertions<ILog, LogAssertions>
-    {
-    }
-}
